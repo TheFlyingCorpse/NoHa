a1f780c5e8bf6a884878459c6ab34a9de11d2f0d
Rune 'TheFlyingCorpse' Darrud <theflyingcorpse@gmail.com>
2012-01-25 23:51:08 +0100

    * Input filters matched successfully with those defined in rules.yml

--- ruleeval.py
+++ ruleeval.py
@@ -54,15 +54,30 @@ class ruleeval:
 		return False
 
 	if not input_delimiter:
-		self.logger.warn("Setting to default delimiter for application because none are set for input: " + str(AppConfig['delimiter']))
+		self.logger.error("Setting to default delimiter for application because none are set for input: " + str(AppConfig['delimiter']))
 		input_delimiter = AppConfig['delimiter']
 
 	if not input_separator:
-		self.logger.warn("Setting to default separator for application because none are set for input: " + str(AppConfig['separator']))
+		self.logger.error("Setting to default separator for application because none are set for input: " + str(AppConfig['separator']))
 		input_separator = AppConfig['separator']
 
+	valid_rules=[]
 	# Fetch rules for application + instance and process the conditions to determine valid ones
-	# Loop
+	for item in i.load_rules_config(YamlConfig):
+
+		self.logger.critical("Rule: " + str(item))
+
+		# Do we have a defined instance(dont skip this rule if not defined)? If instance defined, check if it matches, continue to next rule if no match.
+		if item['instance']:
+			if item['instance'] != instance:
+				continue
+
+		# For easier readability
+		rule_conditions=item['conditions']
+
+		for rule_item in rule_conditions:
+			filter_match=self.evaluate_input_vs_conditions(input,input_delimiter,input_separator,rule_item)
+			print("Filter result: " + str(filter_match))
 #		valid_conditions =  self.evaluate_input_vs_conditions(input,input_delimiter,input_separator,condition_list,condition_delimiter,condition_separator)
 #		is valid_conditions:
 			# Determine if any contacts are set as on-call or shift users.
@@ -72,35 +87,41 @@ class ruleeval:
 			# Determine if contact on-call
 			# More messy code!
 	return True
-	
 
-    def evaluate_input_vs_conditions(self, input, input_separator, rules, rules_separator):
+    def evaluate_input_vs_conditions(self, input, input_delimiter, input_separator, rule_conditions_raw):
         """
         Evaluate input vs conditions
         return True if its a valid Rule (what needs to be found is found, and what is not to be found is not found (-: )
         return False if its not a match
         """
         # Convert input string to list
-        input_list = self.split_by_separator(input, input_separator)
+        input_list = self.split_by_separator(input, input_delimiter)
+
+        # Convert rule to sth edible(list)
+        rule_conditions = self.split_by_separator(rule_conditions_raw,"\n")
 
         # Iterate through rules, and try to match against the input
-        for rule_condition in rules:
-            if not self.process_lists_to_rule_condition(rule_condition,input_list):
+        for item in rule_conditions:
+            self.logger.debug("rule_condition_raw: " + str(item))
+            if not self.process_lists_to_rule_condition(input_list,input_separator,item):
+                self.logger.error("input does not match condition")
                 return False
         return True
-
-    def process_lists_to_rule_condition(self, rule_condition, input_list):
+	
+    def process_lists_to_rule_condition(self, input, input_separator, rule_condition_raw):
         """
         Processes a raw rule_condition to a given list and condition
         """
-        # Split up the rule into name, operator anv values.
-        (r_name, r_operator, r_values) = self.split_by_separator_return_first_second_and_third(rule_condition,';')
+        # Split up the rule into name, operator anv values. (Default values for rule handling for now)
+        self.logger.debug("rule_condition_raw: " + str(rule_condition_raw))
+        (r_name, r_operator, r_values) = self.split_by_separator_return_first_second_and_third(rule_condition_raw,";",",")
 
+#        input_list = self.split_by_separator_return_first_and_second(input,input_delimiter,input_separator)
         # Treat each entry in the input_list as items.
-        for input_item in input_list:
-
+        for item in input:
             # Name = value from input, per name, eg: HOSTGROUPS=linux-srv,postfix-srv
-            (i_name, i_values) = self.split_by_separator_return_first_and_second(input_item,'=')
+            self.logger.debug("item: " + str(item))
+            (i_name, i_values) = self.split_by_separator_return_first_and_second(item,'=',input_separator)
 
             # Check if i_name matches r_name, ex: HOSTGROUPS to HOSTGROUPS, then continue
             if i_name == r_name:
@@ -111,6 +132,7 @@ class ruleeval:
                     continue
                 else:
                     # A rule condition failed.
+                    self.logger.error("condition failed for rule, '" + str(r_values) + " does not match '" + str(i_values) + "'")
                     return False
 
         # If everything else goes to plan, return True.
@@ -141,9 +163,8 @@ class ruleeval:
         """
         # Loop over all input to find matching rules or not.
         for input_item in input_list:
-            #print("")
             match_found= self.regex_match_bool(condition, input_item)
-            #print("Match found" + str(match_found) + "operator: " + str(operator))
+            
             if self.result_decider(operator, match_found):
                 return True
             else:
@@ -156,10 +177,14 @@ class ruleeval:
         """
         Do regex matching of strings, return a Bool type as result.
         """
+        self.logger.error("string1: " + str(string1))
+        self.logger.error("string2: " + str(string2))
         match = re.match(string1, string2)
         if match:
+            self.logger.error("match from regex is True")
             return True
         else:
+            self.logger.error("match from regex is False")
             return False
 
     def result_decider(self, operator, match_found):
@@ -193,31 +218,48 @@ class ruleeval:
         """
         Returns the first entry in list
         """
+        self.logger.debug("string: " + str(string))
+        self.logger.debug("separator: " + str(separator))
         list = string.split(separator)
+        self.logger.debug("list[0]: " + str(list[0]))
         return list[0]
 
     def split_by_separator_return_second(self, string, separator):
         """
         Returns the second entry in list
         """
+        self.logger.debug("string: " + str(string))
+        self.logger.debug("separator: " + str(separator))
         list = string.split(separator)
+        self.logger.debug("list[1]: " + str(list[1]))
         return list[1]
 
-    def split_by_separator_return_first_and_second(self, string, separator):
+    def split_by_separator_return_first_and_second(self, string, first_separator, second_separator):
         """
         Returns the first and second entry in list
         """
-        list1 = string.split(separator)
-        list2 = list1[1].split(",")
+        self.logger.debug("string: " + str(string))
+        self.logger.debug("first_separator: " + str(first_separator))
+        self.logger.debug("second_separator: " + str(second_separator))
+        list1 = string.split(first_separator)
+        self.logger.debug("list1[0]: " + str(list1[0]))
+        list2 = list1[1].split(second_separator)
+        self.logger.debug("list2: " + str(list2))
 
         return (list1[0], list2)
 
-    def split_by_separator_return_first_second_and_third(self, string, separator):
+    def split_by_separator_return_first_second_and_third(self, string, first_separator, second_separator):
         """
-        Returns the first, second and third entry in list
+        Returns the first, second and third(rest) entry in list
         """
-        list1 = string.split(separator)
-        list2 = list1[2].split(",")
+        list1 = string.split(first_separator)
+        self.logger.debug("string: " + str(string))
+        self.logger.debug("first_separator: " + str(first_separator))
+        self.logger.debug("second_separator: " + str(second_separator))
+        self.logger.debug("list1[0]: " + str(list1[0]))
+        self.logger.debug("list1[1]: " + str(list1[1]))
+        list2 = list1[2].split(second_separator)
+        self.logger.debug("list2: " + str(list2))
 
         return (list1[0], list1[1], list2)
 
